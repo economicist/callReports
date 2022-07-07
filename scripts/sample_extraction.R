f28d65420755a304816c2878ff9d1b7b4463ad12
Robert C. Roper <4733099+economicist@users.noreply.github.com>
2022-07-07 13:22:38 -0700

    Commented out the correct line in `sample_extraction.R`

--- scripts/sample_extraction.R
+++ scripts/sample_extraction.R
@@ -3,8 +3,8 @@ if (rstudioapi::isAvailable()) {
   setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 }
 
-# library(callReports)
-devtools::document('~/code/callReports/')
+library(callReports)
+# devtools::document('~/code/callReports/')
 
 extract_chifed_zips(db_connector_sqlite(), get_chifed_zip_dir())
 extract_ffiec_zips(db_connector_sqlite(), get_ffiec_zip_dir())
