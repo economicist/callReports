42315c733857c4d901a9284879307e06479603c5
Robert C. Roper <4733099+economicist@users.noreply.github.com>
2022-07-07 17:42:49 -0700

    Cleaned up sample script and updated description

--- scripts/sample_extraction.R
+++ scripts/sample_extraction.R
@@ -1,10 +1,5 @@
-if (rstudioapi::isAvailable()) {
-  rm(list = ls(all = TRUE))
-  setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-}
-
+rm(list = ls(all.names = TRUE))
 library(callReports)
-# devtools::document('~/code/callReports/')
 
 # Latest feature: Persistent configuration with interactive selections
 #
@@ -12,7 +7,7 @@ library(callReports)
 # this library, as the `getter` functions will automatically call them when they
 # need values to get.
 #
-# Do, howver, uncomment and run the appropriate line below if you ever need to
+# Do, however, uncomment and run the appropriate line below if you ever need to
 # change the location or name of the database file, or either of the folders
 # where the ZIP files are stored:
 #
@@ -20,6 +15,8 @@ library(callReports)
 # set_chifed_zip_dir()
 # set_ffiec_zip_dir()
 
+# The `get_*()` functions used as arguments below can all be used on their own
+# if you ever want to know the current database and ZIP folder configuration.
 create_new_sqlite_db(get_sqlite_filename(), overwrite = TRUE, confirm = FALSE)
-# extract_chifed_zips(db_connector_sqlite(), get_chifed_zip_dir())
+extract_chifed_zips(db_connector_sqlite(), get_chifed_zip_dir())
 extract_ffiec_zips(db_connector_sqlite(), get_ffiec_zip_dir())
