1e6f2b69308139aa4ca65eb393592721db223bb3
Robert C. Roper <4733099+economicist@users.noreply.github.com>
2022-07-07 18:13:04 -0700

    Update sample script

--- scripts/sample_extraction.R
+++ scripts/sample_extraction.R
@@ -1,6 +1,6 @@
 rm(list = ls(all.names = TRUE))
-# library(callReports)
-devtools::document('~/code/callReports')
+library(callReports)
+
 # Latest feature: Persistent configuration with interactive selections
 #
 # Not necessary to run the following `setter` functions the first time you use
@@ -10,13 +10,15 @@ devtools::document('~/code/callReports')
 # Do, however, uncomment and run the appropriate line below if you ever need to
 # change the location or name of the database file, or either of the folders
 # where the ZIP files are stored:
-#
+
 # set_sqlite_filename()
 # set_chifed_zip_dir()
 # set_ffiec_zip_dir()
 
+
 # The `get_*()` functions used as arguments below can all be used on their own
 # if you ever want to know the current database and ZIP folder configuration.
+
 create_new_sqlite_db(get_sqlite_filename(), overwrite = TRUE, confirm = FALSE)
 # extract_chifed_zips(db_connector_sqlite(), get_chifed_zip_dir())
 extract_ffiec_zips(db_connector_sqlite(), get_ffiec_zip_dir())
